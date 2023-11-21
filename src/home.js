08246da11634333b9709f165331f0a8e7dfacdf0
rizk <rizkdotai@gmail.com>
2023-11-21 17:38:24 +0200

    Setup basic structure

---
+++ src/home.js
@@ -0,0 +1,13 @@
+const createHomePage = () => {
+    const content = document.querySelector('#content');
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content')
+
+    const headline = document.createElement('h1');
+    headline.classList.add('text-4xl', 'text-blue-700', 'bg-orange-300')
+    headline.textContent = 'My TODO App'
+    pageContent.appendChild(headline);
+    content.appendChild(pageContent)
+}
+
+export default createHomePage;
