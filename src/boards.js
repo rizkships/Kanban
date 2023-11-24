dff4add88c83f48f6c72f071be17bd7628948ae8
rizk <rizkdotai@gmail.com>
2023-11-24 13:23:07 +0200

    Add boards module

---
+++ src/boards.js
@@ -0,0 +1,23 @@
+import Card from './card.js'
+
+const Board = (name) => {
+    let cards = [];
+
+    const addCard = (cardName) => {
+        const newCard = Card(cardName);
+        cards.push(newCard);
+        return newCard;
+    };
+
+    const getCards = () => {
+        return cards;
+    };
+
+    return {
+        name,
+        addCard,
+        getCards,
+    };
+};
+
+export default Board;
