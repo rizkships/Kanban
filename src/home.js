27a84b5af5eea8e8ca989b901d339a2393568a96
rizk <rizkdotai@gmail.com>
2023-11-25 17:40:25 +0200

    Add event listener for new boards

--- src/home.js
+++ src/home.js
@@ -32,12 +32,33 @@ const createHomePage = () => {
         return boardBtn;
     };
 
+    // Add event listener for the "Add new board" button
+
+    newBoardBtn.addEventListener('click', () => {
+        const boardName = prompt('Enter the name for the new board:');
+        if (boardName) {
+            const newBoard = boardManager(boardName);
+            // Update UI to display the new board
+            updateBoardUI();
+            console.log(`New board created: ${newBoard.name}`);
+        // why do we not have const newBoard = boardManager.createBoard(boardName);
+        }
+    })
+
+    
     const newBoardBtn = document.createElement('button');
     newBoardBtn.textContent = '+  Add new board'
     pageContent.appendChild(newBoardBtn)
     content.appendChild(pageContent)
 
-    // Add event listener for the "Add new board" button
+   
+}
+
+
+export default createHomePage;
+
+ // Add event listener for the "Add new board" button
+    /*
   newBoardBtn.addEventListener('click', () => {
     // Prompt the user to enter the name for the new board
     const boardName = prompt('Enter the name for the new board:');
@@ -49,9 +70,4 @@ const createHomePage = () => {
     // You can update the UI or perform additional actions if needed
     console.log(`New board created: ${newBoard.name}`);
     }
-  });
-}
-
-
-export default createHomePage;
-
+  }); */
